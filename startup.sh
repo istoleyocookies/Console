d4cbc9f1b3071fb33edcb99301a4f39819f348dd
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-05-15 09:56:42 -0700

    fixing a really really dumb mistake

--- startup.sh
+++ startup.sh
@@ -7,5 +7,4 @@ if [ ! -f /opt/faction/certs/api.key ]; then
   openssl req -newkey rsa:4096 -x509 -keyout /opt/faction/certs/api.key -out /opt/faction/certs/api.pem -days 365 -nodes -subj "/O='The Faction Team'/OU='FactionC2'/CN='factionc2'"
 fi
 >&2 echo "Waiting for RabbitMQ to start..."
-#/bin/bash ./wait-for-it.sh mq:5672 -- nginx -g 'daemon off;'
-ls /app
+/bin/bash ./wait-for-it.sh mq:5672 -- nginx -g 'daemon off;'
